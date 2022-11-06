793e3d64184d3634afff5505e23142a8377d017f
Stefan Eder <darkmaster.eder@gmail.com>
2022-11-06 20:54:38 +0100

    extend IUserService interface

--- UserService.Core/Interfaces/IUserService.cs
+++ UserService.Core/Interfaces/IUserService.cs
@@ -8,4 +8,10 @@ public interface IUserService
     public Task<User>? GetUserById(int id);
     public Task<User> GetUserByEmail(string eMail);
     public Task<User> GetUserByUserName(string userName);
+
+    public Task<bool> CreateUser(User user);
+
+    public Task<User> UpdateUserWithId(int userId, User user);
+
+    public Task<bool> DeleteUserWithId(int userId);
 }
