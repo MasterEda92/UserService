00c09f8b2181ff7664d5994939785a5916ae759c
Stefan Eder <darkmaster.eder@gmail.com>
2022-11-11 14:40:30 +0100

    add test class for UserStoreDb

---
+++ UserService.Tests/UnitTests/TestUserStoreDb.cs
@@ -0,0 +1,25 @@
+namespace UserService.Tests.UnitTests;
+
+public class TestUserStoreDb
+{
+    #region ctor and fields
+
+    public TestUserStoreDb()
+    {
+        
+    }
+
+    #endregion
+
+    #region tests
+
+    
+
+    #endregion
+
+    #region private helper functions
+
+    
+
+    #endregion
+}
