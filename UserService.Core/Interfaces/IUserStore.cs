6da33917fa231be570a7d1b409cd7ee27e799688
Stefan Eder <darkmaster.eder@gmail.com>
2022-11-14 09:27:59 +0100

    first implementation of UserStoreDb for basic research. Tests will be added and the implementation changed as tests will be written.

--- UserService.Core/Interfaces/IUserStore.cs
+++ UserService.Core/Interfaces/IUserStore.cs
@@ -7,7 +7,7 @@ public interface IUserStore
     public Task<User> AddUser(User newUser);
     public Task<User> UpdateUser(User user);
     public Task<User> DeleteUserWithId(int id);
-    public Task Save();
+    public Task<int> Save();
     public Task<User> GetUserWithId(int id);
     public Task<IEnumerable<User>> GetAllUsers();
     public Task<IEnumerable<User>> GetUsers(Func<User, bool> predicate);
--- UserService.DbAccess/Services/UserStoreDb.cs
+++ UserService.DbAccess/Services/UserStoreDb.cs
@@ -1,42 +1,74 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using UserService.Core.Entities;
+using UserService.Core.Exceptions;
 using UserService.Core.Interfaces;
+using UserService.DbAccess.DbContext;
+using UserService.DbAccess.Models;
 
 namespace UserService.DbAccess.Services;
 
 public class UserStoreDb : IUserStore
 {
-    public Task<User> AddUser(User newUser)
+    private readonly UserServiceDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UserStoreDb(UserServiceDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public async Task<User> AddUser(User newUser)
+    {
+        var entry = await _context.AddAsync(_mapper.Map<UserModel>(newUser));
+        return _mapper.Map<User>(entry.Entity);
+
     }
 
     public Task<User> UpdateUser(User user)
     {
-        throw new NotImplementedException();
+        var updatedUser = _context.Users.Update(_mapper.Map<UserModel>(user)).Entity;
+        if (updatedUser is null)
+            throw new UserNotFoundException(); // TODO: Prüfen ob hier noch andere Fälle auftreten können
+        
+        return Task.FromResult(_mapper.Map<User>(updatedUser));
     }
 
-    public Task<User> DeleteUserWithId(int id)
+    public async Task<User> DeleteUserWithId(int id)
     {
-        throw new NotImplementedException();
+        var user = await _context.Users.FindAsync(id);
+        if (user is null)
+            throw new UserNotFoundException();
+        
+        var delUser = _context.Users.Remove(_mapper.Map<UserModel>(user)).Entity;
+        return _mapper.Map<User>(delUser);
     }
 
-    public Task Save()
+    public async Task<int> Save()
     {
-        throw new NotImplementedException();
+        return await _context.SaveChangesAsync();
     }
 
-    public Task<User> GetUserWithId(int id)
+    public async Task<User> GetUserWithId(int id)
     {
-        throw new NotImplementedException();
+        var user = await _context.Users.SingleOrDefaultAsync(user => user.Id == id);
+        if (user is null)
+            throw new UserNotFoundException();
+        
+        return _mapper.Map<User>(user);
     }
 
-    public Task<IEnumerable<User>> GetAllUsers()
+    public async Task<IEnumerable<User>> GetAllUsers()
     {
-        throw new NotImplementedException();
+        IEnumerable<UserModel> users = await _context.Users.ToListAsync();
+        return _mapper.Map<IEnumerable<User>>(users);
     }
 
-    public Task<IEnumerable<User>> GetUsers(Func<User, bool> predicate)
+    public async Task<IEnumerable<User>> GetUsers(Func<User, bool> predicate)
     {
-        throw new NotImplementedException();
+        var pred = _mapper.Map<Func<UserModel, bool>>(predicate);
+        IEnumerable<UserModel> users = _context.Users.Where(pred).ToList();
+        return await Task.FromResult(_mapper.Map<IEnumerable<User>>(users));
     }
 }
